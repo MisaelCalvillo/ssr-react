62f2457f6eabc0a0361b3012b6b252e8dfe7dd3f
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2019-09-11 10:13:28 -0500

    Usa url-loader para la carga de archivos

--- url-loader/src/js/components/App.js
+++ url-loader/src/js/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from './icon.png';
 
 function App() {
   return (
     <div>
       una app de react con nuestro propio webpack
-      <img src="./icon.jpg" />
+      <img src={logo} />
     </div>
   )
 }
